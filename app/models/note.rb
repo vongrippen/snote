9ec44b3d0e6b43e425a848e1298c1f13d26aed8d
Demian Gemperli <demian.gemperli@gmail.com>
2011-06-25 01:05:37 +0200

    Postgres fix for search by tags

--- app/models/note.rb
+++ app/models/note.rb
@@ -17,9 +17,10 @@ class Note < ActiveRecord::Base
   end
 
   def self.with_tag(tag, current_user_id)
+    cols = self.column_names.collect {|c| "notes.#{c}"}.join(",") #Postgres fix
     all(:conditions => ["tags.name LIKE ? AND notes.user_id = ?","%#{eval("tag")}%", current_user_id],
         :joins => "INNER JOIN taggings ON taggings.taggable_id = notes.id
-          INNER JOIN tags ON taggings.tag_id = tags.id",:group => 'notes.id')
+          INNER JOIN tags ON taggings.tag_id = tags.id",:group => cols)
   end
 
   def self.find_public_note(user_id, note_id)
