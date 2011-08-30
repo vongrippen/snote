8a9d53df22b47ced923b892ddb39dfed1477c120
Mike Cochran <mcochran@linux.com>
2011-08-30 14:40:32 -0500

    Surely Railscasts has it right?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,9 @@ module ApplicationHelper
     render :partial => 'shared/analytics' if Rails.env == 'test'
   end
   
-  def textilize(text)
-    Textilizer.new(text).to_html unless text.blank?
+  def coderay(text)
+    text.gsub(/\<code( lang="(.+?)")?\>(.+?)\<\/code\>/m) do
+      content_tag("notextile", CodeRay.scan($3, $2).div(:css => :class))
+    end
   end
 end
